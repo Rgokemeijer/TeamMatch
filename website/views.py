d8208d1503446aba70c047903da53f508528a88b
Russellgoke <31809779+Russellgoke@users.noreply.github.com>
2023-02-16 15:04:20 -0800

    Added button on homepage to a roster_create page

--- website/views.py
+++ website/views.py
@@ -1,13 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
+from .models import Student_Roster
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        return redirect(url_for('views.roster_create'))
     return render_template("home.html", user=current_user) #in template check if current_user is authenticated
 
-@views.route('/test', methods=['GET', 'POST'])
-def temp():
-    return render_template("roster_create.html", user=current_user)
+@views.route('/create_grouping/upload_roster', methods=['GET', 'POST'])
+def roster_create():
+    roster = Student_Roster(email="test", first_name="fname", last_name="lastname")
+    db.session.add(roster) 
+    db.session.commit() # adds to DB
+    students = Student_Roster.query.all()
+    return render_template("roster_create.html", user=current_user, students=students)
