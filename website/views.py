966e6c9ae95c3796c64b34ee7d77c023997bd6b5
Sara Vanaki <svanaki@sandiego.edu>
2023-02-21 15:25:25 -0800

    new create groupings button

--- website/views.py
+++ website/views.py
@@ -1,13 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods =["GET", "POST"])
 @login_required
 def home():
-    return render_template("home.html", user=current_user) #in template check if current_user is authenticated
+    if request.method == 'POST':
+        return redirect(url_for('views.groupings', user = current_user))
+    elif request.method == 'GET':
+        return render_template("home.html", user=current_user) #in template check if current_user is authenticated
 
 @views.route('/test', methods=['GET', 'POST'])
 def temp():
+    return render_template("roster_create.html", user=current_user)
+
+@views.route('/groupings', methods=['GET',"POST"])
+@login_required
+def groupings():
     return render_template("roster_create.html", user=current_user)
